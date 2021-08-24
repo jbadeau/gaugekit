80af740a9320c7f8afe4c3f3a128b73dc7887d43
Jose Badeau <jose.badeau@gmail.com>
2021-08-24 10:40:05 +0200

    added webdriver manager support

--- gauge-browser/src/test/java/org/gaugekit/browser/supplier/LocalWebDriverSupplierTest.java
+++ gauge-browser/src/test/java/org/gaugekit/browser/supplier/LocalWebDriverSupplierTest.java
@@ -5,43 +5,36 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.NoSuchSessionException;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assumptions.assumeThat;
 import static org.gaugekit.browser.supplier.BrowserType.CHROME;
 import static org.gaugekit.browser.supplier.BrowserType.FIREFOX;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
 
 class LocalWebDriverSupplierTest {
 
-    private static final Map<BrowserType, String> COMMANDS;
+    private static final Map<BrowserType, String> COMMANDS = Map.of(
+            CHROME, "google-chrome",
+            FIREFOX, "firefox");
 
-    private static final Map<BrowserType, Capabilities> HEADLESS;
-
-    static {
-        COMMANDS = new HashMap<>(2);
-        COMMANDS.put(CHROME, "chromedriver");
-        COMMANDS.put(FIREFOX, "geckodriver");
-
-        HEADLESS = new HashMap<>(2);
-        HEADLESS.put(CHROME, new ChromeOptions().setHeadless(true));
-        HEADLESS.put(FIREFOX, new FirefoxOptions().setHeadless(true));
-
-    }
+    private static final Map<BrowserType, Capabilities> HEADLESS = Map.of(
+            CHROME, new ChromeOptions().setHeadless(true),
+            FIREFOX, new FirefoxOptions().setHeadless(true));
 
     @ParameterizedTest(name = "get returns a {0}")
     @EnumSource(BrowserType.class)
     void getTest1(BrowserType browserType) throws IOException, InterruptedException {
-        assumeTrue(COMMANDS.containsKey(browserType));
-        assumeTrue(new ProcessBuilder("where", COMMANDS.get(browserType)).start().waitFor() == 0);
+        assumeThat(COMMANDS).containsKey(browserType);
+        assumeThat(new ProcessBuilder("which", COMMANDS.get(browserType)).start().waitFor()).isEqualTo(0);
 
-        final LocalWebDriverSupplier localWebDriverSupplier = new LocalWebDriverSupplier(browserType, HEADLESS.get(browserType));
+        final var localWebDriverSupplier = new LocalWebDriverSupplier(browserType, HEADLESS.get(browserType));
 
         assertThat(localWebDriverSupplier.get())
                 .isNotNull()
@@ -53,20 +46,33 @@ class LocalWebDriverSupplierTest {
     @Test
     @DisplayName("get caches the WebDriver")
     void getTest2() throws IOException, InterruptedException {
-        assumeTrue(COMMANDS.containsKey(FIREFOX));
-        assumeTrue(new ProcessBuilder("where", COMMANDS.get(FIREFOX)).start().waitFor() == 0);
+        assumeThat(COMMANDS).containsKey(FIREFOX);
+        assumeThat(new ProcessBuilder("which", COMMANDS.get(FIREFOX)).start().waitFor()).isEqualTo(0);
 
-        final LocalWebDriverSupplier localWebDriverSupplier = new LocalWebDriverSupplier(FIREFOX, HEADLESS.get(FIREFOX));
+        final var localWebDriverSupplier = new LocalWebDriverSupplier(FIREFOX, HEADLESS.get(FIREFOX));
 
-        final WebDriver webDriver = localWebDriverSupplier.get();
+        final var webDriver = localWebDriverSupplier.get();
         assertThat(webDriver).isSameAs(localWebDriverSupplier.get());
 
         localWebDriverSupplier.close();
     }
 
     @Test
-    @DisplayName("close does nothing when no WebDriver was created")
-    void closeTest1() {
-        new LocalWebDriverSupplier(FIREFOX).close();
+    @DisplayName("close quits the WebDriver")
+    void closeTest1() throws IOException, InterruptedException {
+        assumeThat(COMMANDS).containsKey(FIREFOX);
+        assumeThat(new ProcessBuilder("which", COMMANDS.get(FIREFOX)).start().waitFor()).isEqualTo(0);
+
+        final var localWebDriverSupplier = new LocalWebDriverSupplier(FIREFOX, HEADLESS.get(FIREFOX));
+
+        final var webDriver = localWebDriverSupplier.get();
+
+        assertThatNoException()
+                .isThrownBy(() -> webDriver.manage().window().getSize());
+
+        localWebDriverSupplier.close();
+
+        assertThatExceptionOfType(NoSuchSessionException.class)
+                .isThrownBy(() -> webDriver.manage().window().getSize());
     }
 }
